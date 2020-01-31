3bbd608f85c4062c4e3d231f8a0e7ce7b3a5473a
Wan <wmadzha11@hotmail.com>
2020-01-31 08:21:05 +0800

    Add BsonID As Primary Key

--- MeganMagoo.Business.DataAccess/NoSqlDbContext/BusinessModel_1.cs
+++ MeganMagoo.Business.DataAccess/NoSqlDbContext/BusinessModel_1.cs
@@ -1,8 +1,13 @@
 ﻿using MeganMagoo.MongoDb.Core;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace MeganMagoo.Business.DataAccess.NoSqlDbContext
 {
     public class BusinessModel_1 : IMongoEntity
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string BusinessModelName { get; set; }
     }
--- MeganMagoo.Business.DataAccess/NoSqlDbContext/BusinessModel_2.cs
+++ MeganMagoo.Business.DataAccess/NoSqlDbContext/BusinessModel_2.cs
@@ -1,9 +1,12 @@
 ﻿using MeganMagoo.MongoDb.Core;
-
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace MeganMagoo.Business.DataAccess.NoSqlDbContext
 {
     public class BusinessModel_2 : IMongoEntity
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string BusinessModelName { get; set; }
     }
